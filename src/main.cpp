5087d0197035d2af9d2d801c35b3b23b77b6c31e
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-10-07 14:48:14 +0200

    feat: Add wifi code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include "WiFi.h"
+
+
+// WiFi credentials.
+const char* WIFI_SSID = "iPhone de Jory";
+const char* WIFI_PASS = "Jory38360@@";
+
 
 // Constants - ESP32 specific
 #define DHTPIN 4        // DHT22 connected to GPIO4 (common choice for ESP32)
@@ -47,6 +54,51 @@ void setup() {
   delay(2500);
   Serial.println("Ready!");
   Serial.println("================================\n");
+
+  Serial.begin(9600);
+    // Giving it a little time because the serial monitor doesn't
+    // immediately attach. Want the firmware that's running to
+    // appear on each upload.
+    delay(2000);
+
+    Serial.println();
+    Serial.println("Running Firmware.");
+
+    // Connect to Wifi.
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(WIFI_SSID);
+
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    Serial.println("Connecting...");
+
+    while (WiFi.status() != WL_CONNECTED) {
+      // Check to see if connecting failed.
+      // This is due to incorrect credentials
+      if (WiFi.status() == WL_CONNECT_FAILED) {
+        Serial.println("Failed to connect to WIFI. Please verify credentials: ");
+        Serial.println();
+        Serial.print("SSID: ");
+        Serial.println(WIFI_SSID);
+        Serial.print("Password: ");
+        Serial.println(WIFI_PASS);
+        Serial.println();
+      }
+      delay(5000);
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("Hello World, I'm connected to the internets!!");
 }
 
 void loop() {
